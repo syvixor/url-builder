75763fafd54304ae216cd9f50b6f79e146bd2135
syvixor <syvixor@proton.me>
2025-08-19 12:41:50 +0100

    refactor: update api response handling to match new response structure

--- server/api/icons.get.ts
+++ server/api/icons.get.ts
@@ -4,7 +4,13 @@ interface IIcons {
     url: string;
 }
 
+interface IResponse {
+    statusCode: number;
+    statusMessage: string;
+    data: IIcons[];
+}
+
 export default defineEventHandler(async () => {
-    const data = await $fetch<IIcons[]>("https://skills.syvixor.com/api/icons/all");
-    return data
+    const data = await $fetch<IResponse>("https://skills.syvixor.com/api/icons/all");
+    return data.data
 });
