5fc2ec24d7cc93596a8ac41ed9b2298fc39dfe28
syvixor <syvixor@proton.me>
2025-10-27 09:13:09 +0100

    chore: move compatibility date to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["@nuxt/ui", "@vueuse/nuxt"],
+    compatibilityDate: "latest",
     devtools: { enabled: true },
     colorMode: { preference: "dark" },
     css: ["~/assets/css/global.css"]
